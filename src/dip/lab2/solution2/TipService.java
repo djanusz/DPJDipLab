1b1ba230b9dcbf294f030ed6c085ae40d464fadd
djanusz <djanusz@L11703038599.wctc.msft>
2012-09-26 19:22:59 -0500

    Created TipCalculatorStrategy interface and lower- levels BaggageCalculatorStrategy and RestaurantTipStrategy classes. Also created the TipService class. Still working on TipService class and needs to be tested with the Startup class.

---
+++ src/dip/lab2/solution2/TipService.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dip.lab2.solution2;
+
+/**
+ *
+ * @author djanusz
+ */
+public class TipService {
+    
+}
